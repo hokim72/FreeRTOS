15b52e83849dee0de1375ae6453d2740ea02716e
Hyunok Kim <hokim72@gmail.com>
2015-10-11 22:00:40 +0900

    Add Boot examples

---
+++ Book/Example13/Example13.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "uartio.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
+xSemaphoreHandle xCountingSemaphore;
+
+static void vPeriodicTask(void* pvParameters)
+{
+	// As per most tasks, this task is implemented within an infinite loop.
+	for (;;)
+	{
+		// This task is just used to 'simulate' an interrupt. This is done by
+		// periodically generating a software interrupt.
+		vTaskDelay(500/portTICK_RATE_MS);
+
+		// Generate the interrupt, printing a message both before hand and
+		// afterwards so the sequence of execution is evident from the output.
+		printf("Periodic task - About to generate an interrupt.\n");
+		fflush(stdout);
+		PORTD &= ~(1 << PD0);
+		PORTD |= (1 << PD0);
+		printf("Periodic task - Interrupt generated.\n\n");
+		fflush(stdout);
+	}
+}
+
+static void vHandlerTask(void* pvParameters)
+{
+	// As per most tasks, this task is implemented within an infinite loop.
+
+	// Task the semaphore once to start with so the semaphore is empty before
+	// the infinite loop is entered. The semaphore was created before the 
+	// scheduler was started so before this task ran for the first time.
+	xSemaphoreTake(xCountingSemaphore, 0);
+
+	for (;;)
+	{
+		// Use the semaphore to wait for the event. The task blocks
+		// indefinitely meaning thin function call will only return once the
+		// semaphore has been successfully obtained - so there is no need to
+		// check the returned value.
+		xSemaphoreTake(xCountingSemaphore, portMAX_DELAY);
+
+		// To get here event must have occured. Process the event (in this
+		// case we just print out a message).
+		printf("Handler task - Processing event.\n");
+		fflush(stdout);
+	}
+}
+
+ISR(INT0_vect)
+{
+	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+	// 'Give' the semaphore multiple times. The first will unblock the
+	// handler task, the following 'gives' are to demonstrate that the
+	// semaphore latches the events to allow the handler task to process
+	// them in turn without any events getting lost. This simulates multiple
+	// interrupts being taken by the processor, even though in this case
+	// the events are simulated within a single interrupt occurrence.
+	xSemaphoreGiveFromISR(xCountingSemaphore, &xHigherPriorityTaskWoken);
+	xSemaphoreGiveFromISR(xCountingSemaphore, &xHigherPriorityTaskWoken);
+	xSemaphoreGiveFromISR(xCountingSemaphore, &xHigherPriorityTaskWoken);
+
+	if (xHigherPriorityTaskWoken == pdTRUE)
+	{
+		// Giving the semaphore may have unblocked a task - if it did and the
+		// unblocked task has a priority equal to or above the currently
+		// executing task then xHigherPriorityTaskWoken will have been set to
+		// pdTRUE and vPortYield() will force a context switch to the newly
+		// unblocked higher priority task.
+
+		// NOTE: The syntax for forcing a context switch within an ISR varies 
+		// between FreeRTOS ports.
+		vPortYield();
+	}
+}
+
+int main(void)
+{
+	uartIOInit();
+	stdout = &uartIOOutput;
+
+	DDRD |= (1 << PD0);
+	PORTD &= ~(1 << PD0);
+
+	// Before a semaphore is used it must be explicitly created. In this example
+	// a counting semaphore is created. The semaphore is created to have 
+	// a maximum count value of 10, and an initial count value of 0.
+	xCountingSemaphore = xSemaphoreCreateCounting(10,0);
+
+	// Check the semaphore was created successfully.
+	if (xCountingSemaphore != NULL)
+	{
+		// Enable the software interrupt and set its priority.
+		EIMSK |= (1 << INT0);
+		EICRA |= (1 << ISC00) | (1 << ISC01);
+		sei();
+
+		// Create the 'handler' task. This is the task that will be synchronized
+		// with the interrupt. The handler task is created with a  high priority
+		// to ensure it runs immediately after interrupt exits. In this case a
+		// priority of 3 is chosen.
+		xTaskCreate(vHandlerTask, "Handler", configMINIMAL_STACK_SIZE*2, NULL,
+					tskIDLE_PRIORITY+3, NULL);
+
+		// Create the task that will periodically generate a software interrupt.
+		// This is created with a priority below the handler task to ensure it 
+		// will get preempted each time the handler task exits the Blocked
+		// state.
+		xTaskCreate(vPeriodicTask, "Periodic", configMINIMAL_STACK_SIZE*2, NULL,
+					tskIDLE_PRIORITY+1, NULL);
+
+		// Start the scheduler so the created tasks start executing.
+		vTaskStartScheduler();
+
+	}
+
+	// If all is well we will never reach here as the scheduler will now be
+	// running the tasks. If we do reach here then it is likely that there was
+	// insufficient heap memory available for a resource to be created.
+	for (;;);
+
+	return 0;
+}
---
+++ Book/Example14/Example14.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "uartio.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+// Declare two variable of type QueueHandle. One queue will be read from
+// within an ISR, the other will be written to from within an ISR.
+xQueueHandle xIntegerQueue, xStringQueue;
+
+static void vIntegerGenerator(void* pvParameters)
+{
+	portTickType xLastExecutionTime;
+	unsigned long ulValueToSend = 0;
+	int i;
+
+	// Initialize the variable used by the call to vTaskDelayUntil().
+	xLastExecutionTime = xTaskGetTickCount();
+	for (;;)
+	{
+		// This is a periodic task. Block until it is time to run again.
+		// The task will execute every 200ms.
+		vTaskDelayUntil(&xLastExecutionTime, 200/portTICK_RATE_MS);
+
+		// Send an incrementing number to the queue five times. The values
+		// will be read from the queue by the interrupt service routine.
+		// The interrupt service routine always empties the queue so this
+		// task is guaranteed to be able to write all five values, so
+		// a block time is not required.
+		for (i=0; i<5; i++)
+		{
+			xQueueSendToBack(xIntegerQueue, &ulValueToSend, 0);
+			ulValueToSend++;
+		}
+
+		// Force an interrupt so the interrupt service routine can read the
+		// values from the queue.
+		printf("Generator task - About to generate an interrupt.\n");
+		fflush(stdout);
+		PORTD &= ~(1 << PD0);
+		PORTD |= (1 << PD0);
+		printf("Generator task - Interrupt generated.\n\n");
+		fflush(stdout);
+	}
+}
+
+static void vStringPrinter(void* pvParameters)
+{
+	char* pcString;
+
+	for (;;)
+	{
+		// Block on the queue to wait for data to arrive.
+		xQueueReceive(xStringQueue, &pcString, portMAX_DELAY);
+
+		// Print out the string received.
+		printf(pcString);
+		fflush(stdout);
+	}
+}
+
+ISR(INT0_vect)
+{
+	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+	static unsigned long ulReceivedNumber;
+
+	// The strings are declared static const to ensure they are not allocated
+	// to the interrupt service routine stack, and exist even when the
+	// interrupt service routine is not executing.
+	static const char* pcStrings[] =
+	{
+		"String 0\n",
+		"String 1\n",
+		"String 2\n",
+		"String 3\n"
+	};
+
+	// Loop until the queue is empty.
+	while (xQueueReceiveFromISR(xIntegerQueue, &ulReceivedNumber,
+								&xHigherPriorityTaskWoken) != errQUEUE_EMPTY)
+	{
+		// Truncate the received value to the last two bits (value 0 to 3 inc.),
+		// then send the string that corresponds to the truncated value to the
+		// other queue.
+		ulReceivedNumber &= 0x03;
+		xQueueSendToBackFromISR(xStringQueue,
+								&pcStrings[ulReceivedNumber],
+								&xHigherPriorityTaskWoken);
+	}
+
+	if (xHigherPriorityTaskWoken == pdTRUE)
+	{
+		// Giving the semaphore may have unblocked a task - if it did and the
+		// unblocked task has a priority equal to or above the currently
+		// executing task then xHigherPriorityTaskWoken will have been set to
+		// pdTRUE and vPortYield() will force a context switch to the newly
+		// unblocked higher priority task.
+
+		// NOTE: The syntax for forcing a context switch within an ISR varies 
+		// between FreeRTOS ports.
+		vPortYield();
+	}
+}
+
+int main(void)
+{
+	uartIOInit();
+	stdout = &uartIOOutput;
+
+	DDRD |= (1 << PD0);
+	PORTD &= ~(1 << PD0);
+
+	// Before a queue can be used it must first be created. Create both queues
+	// used by this example. One queue can hold varaiables of type unsigned
+	// long, the other queue can hold variables of type char*. Both queues
+	// can hold a maximum of 10 items. A real application should check the
+	// return values to ensure the queues have successfully created.
+	xIntegerQueue = xQueueCreate(10, sizeof(unsigned long));
+	xStringQueue = xQueueCreate(10, sizeof(char*));
+
+	// Enable the software interrupt and set its priority.
+	EIMSK |= (1 << INT0);
+	EICRA |= (1 << ISC00) | (1 << ISC01);
+	sei();
+
+	// Create the task that uses a queue to pass integers to the interrupt
+	// service routine. The task is created at priority 1.
+	xTaskCreate(vIntegerGenerator, "IntGen", configMINIMAL_STACK_SIZE*2,
+				NULL, tskIDLE_PRIORITY+1, NULL);
+
+	// Create the task the prints out the strings sent to it from the interrupt
+	// service routine. This task is created at the higher priority of 2.
+	xTaskCreate(vStringPrinter, "String", configMINIMAL_STACK_SIZE*2,
+				NULL, tskIDLE_PRIORITY+2, NULL);
+
+	// Start the scheduler so the created tasks start executing.
+	vTaskStartScheduler();
+
+	// If all is well we will never reach here as the scheduler will now be
+	// running the tasks. If we do reach here then it is likely that there was
+	// insufficient heap memory available for a resource to be created.
+	for (;;);
+
+	return 0;
+}
